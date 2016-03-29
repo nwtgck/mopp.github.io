810f761dd30cd39d40230a13cbaa363d9ab0c4e5
mopp <ac.kxp165@gmail.com>
2016-03-29 15:20:37 +0900

    [MOD] github info

--- _moppris/js/index.js
+++ _moppris/js/index.js
@@ -1,14 +1,7 @@
 window.onload = function()
 {
-    const NUM_REPOS = 5;
-    const NUM_ACTIVITIES = 10;
-    const API_URL_PUBLIC_ACTIVITY = 'https://api.github.com/users/mopp/events/public?per_page=' + NUM_ACTIVITIES;
     const API_URL_REPOS = 'https://api.github.com/users/mopp/repos?per_page=60';
-    const DEFAULT_OPTION = {
-        type: 'owner',
-        sort: 'full_name',
-        direction: 'asc'
-    };
+    const NUM_REPOS = 6;
 
     function on_load_repos(results, text_status, jq_xhr)
     {
@@ -34,21 +27,23 @@ window.onload = function()
         for (var i = 0; i < NUM_REPOS; ++i) {
             var repo = repos[i];
             var dom =
-                '<li>' +
-                    '<div class="github-repo-name icon-head-h0">' +
-                        '<i class="fa fa-quote-right"></i>' +
-                        '<a target="_blank" href="' + repo.url + '">' + repo.name + '</a>' +
+                '<div class="github-repo pure-u-24-24 pure-u-md-12-24">' +
+                    '<div class="github-repo-frame">' +
+                        '<div class="github-repo-name icon-head-h0">' +
+                            '<i class="fa fa-quote-right"></i>' +
+                            '<a target="_blank" href="' + repo.url + '">' + repo.name + '</a>' +
+                        '</div>' +
+                        '<div class="github-repo-desc">' + repo.description + '</div>' +
+                        '<span class="github-repo-updated-tag">Update:</span>' +
+                        '<span class="github-repo-updated">' + repo.updated + '</span>' +
+                        '<span class="github-repo-lang-tag">Lang:</span>' +
+                        '<span class="github-repo-lang">' + repo.language + 'C</span>' +
+                        '<div>' +
+                            '<span class="github-repo-star-tag"><i class="fa fa-star"></i></span>' +
+                            '<span class="github-repo-star">' + repo.stars + '</span>' +
+                        '</div>' +
                     '</div>' +
-                    '<div class="github-repo-desc">' + repo.description + '</div>' +
-                    '<span class="github-repo-updated-tag">Update:</span>' +
-                    '<span class="github-repo-updated">' + repo.updated + '</span>' +
-                    '<span class="github-repo-lang-tag">Lang:</span>' +
-                    '<span class="github-repo-lang">' + repo.language + 'C</span>' +
-                    '<div>' +
-                        '<span class="github-repo-star-tag"><i class="fa fa-star"></i></span>' +
-                        '<span class="github-repo-star">' + repo.stars + '</span>' +
-                    '</div>' +
-                '</li>';
+                '</div>';
             doms += dom;
         }
         $('#github-repos > .spinner').replaceWith(doms);
@@ -56,7 +51,11 @@ window.onload = function()
 
     $.ajax({
         url: API_URL_REPOS,
-        data: DEFAULT_OPTION,
+        data: {
+            type: 'owner',
+            sort: 'full_name',
+            direction: 'asc'
+        },
         success: on_load_repos,
         dataType: 'json'
     });
